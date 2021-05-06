1e32beacb09789891cd10dbd7f8eafa6e2305d83
Zil Norvilis <zilasino27@gmail.com>
2021-05-06 15:19:45 +0200

    Add content using js module function

--- src/content.js
+++ src/content.js
@@ -1,13 +1,13 @@
 const whatever = () => {
-  const div = document.getElementById("content");
+  let div = document.getElementById("content");
+  div.innerHTML = "";
   const elements = [
     '<img src="../assets/restaurant.jpg" alt="restaurant image" width="500">',
     "<h3>Best Restaurant Ever!</h3>",
     "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est porro facere, sed placeat quisquam ipsum delectus quod praesentium, amet illum fugiat saepe fuga. Reiciendis ad pariatur architecto consectetur cupiditate. Quasi.</p>",
   ];
-  div += "";
   elements.forEach((el) => {
-    div += el;
+    div.innerHTML += el;
   });
 };
 
